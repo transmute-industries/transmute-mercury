f42da7a3d6b612370e51fc0037d97690f830d37e
Orie Steele <orie@ohs.io>
2017-05-13 11:44:09 -0500

    updates

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 
 A [create react app](/app/README.md) seed project
 
+```
+$ cd app
+$ npm install
+$ npm run welcome
+$ npm run start
+```
--- app/src/App.js
+++ app/src/App.js
@@ -1,18 +1,48 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import 'react-mdl/extra/material.css';
+import 'react-mdl/extra/material.js';
 import './App.css';
+
+import Particles from 'react-particles-js';
+let particles = require('./particles.json')
+
+import {
+  Button,
+  IconButton,
+  Card,
+  CardTitle,
+  CardText,
+  CardMenu,
+  CardActions
+} from 'react-mdl';
+
+const Demo = (props) => (
+  <div className="Demo">
+    <Card shadow={0} style={{ margin: 'auto' }}>
+      <CardTitle style={{ color: '#fff', height: '176px', background: 'url(http://www.getmdl.io/assets/demos/welcome_card.jpg) center / cover' }}>
+        Welcome
+      </CardTitle>
+      <CardText>
+        This React app uses the Transmute Framework to interact with the ethereum blockchain.
+    </CardText>
+      <CardActions border>
+        <Button colored>Get Started</Button>
+      </CardActions>
+      <CardMenu style={{ color: '#fff' }}>
+        <IconButton name="share" />
+      </CardMenu>
+    </Card>
+  </div>
+);
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <Demo/>
+        <div className="Particles">
+          <Particles params={particles} />
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
