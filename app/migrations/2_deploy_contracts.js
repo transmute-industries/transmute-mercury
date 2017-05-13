b1b1e32ae19918bc0b1be1ea65c5af8c225ad457
Orie Steele <orie@ohs.io>
2017-05-13 12:25:18 -0500

    Added transmute cli

--- app/migrations/2_deploy_contracts.js
+++ app/migrations/2_deploy_contracts.js
@@ -1,7 +1,21 @@
+// BEGIN 🦄 Transmute Framework 
+var ConvertLib = artifacts.require("./TransmuteFramework/ConvertLib.sol");
+var MetaCoin = artifacts.require("./TransmuteFramework/MetaCoin.sol");
+
+const transmuteDeployer = function(deployer) {
+  deployer.deploy(ConvertLib);
+  deployer.link(ConvertLib, MetaCoin);
+  deployer.deploy(MetaCoin);
+};
+// END 🐩 Transmute Framework 
+
 var ConvertLib = artifacts.require("./ConvertLib.sol");
 var MetaCoin = artifacts.require("./MetaCoin.sol");
 
 module.exports = function(deployer) {
+	// Patched by Transmute Framework
+	transmuteDeployer(deployer)
+
   deployer.deploy(ConvertLib);
   deployer.link(ConvertLib, MetaCoin);
   deployer.deploy(MetaCoin);
