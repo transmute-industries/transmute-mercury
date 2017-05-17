8dc730f9a530662e344011fd1dd0d71a6065ddf4
Orie Steele <orie@ohs.io>
2017-05-16 20:59:50 -0500

    Fix example

--- src/components/Pages/Home/Stepper/AuthorizeEncounter/AuthorizeEncounterTable/AuthorizeEncounterTable.js
+++ src/components/Pages/Home/Stepper/AuthorizeEncounter/AuthorizeEncounterTable/AuthorizeEncounterTable.js
@@ -1,41 +1,108 @@
 import React, {Component} from 'react'
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
-import MuiEditableTable from "mui-editable-table"
+import {
+  Table,
+  TableBody,
+  TableFooter,
+  TableHeader,
+  TableHeaderColumn,
+  TableRow,
+  TableRowColumn,
+} from 'material-ui/Table'
+import TextField from 'material-ui/TextField'
+import Toggle from 'material-ui/Toggle'
 
-export default class AuthorizeEncounterTable extends Component {
+
+const tableData = [
+  {
+    name: 'John Smith',
+    status: 'Employed',
+  },
+  {
+    name: 'Randal White',
+    status: 'Unemployed',
+  },
+  {
+    name: 'Stephanie Sanders',
+    status: 'Employed',
+  },
+  {
+    name: 'Steve Brown',
+    status: 'Employed',
+  },
+  {
+    name: 'Joyce Whitten',
+    status: 'Employed',
+  },
+  {
+    name: 'Samuel Roberts',
+    status: 'Employed',
+  },
+  {
+    name: 'Adam Moore',
+    status: 'Employed',
+  },
+]
+
+export default class TableExampleComplex extends Component {
+  state = {
+    fixedHeader: false,
+    fixedFooter: false,
+    stripedRows: false,
+    showRowHover: false,
+    selectable: true,
+    multiSelectable: false,
+    enableSelectAll: false,
+    deselectOnClickaway: true,
+    showCheckboxes: true
+  }
+
+  handleToggle = (event, toggled) => {
+    this.setState({
+      [event.target.name]: toggled,
+    })
+  }
+
+  handleChange = (event) => {
+    this.setState({height: event.target.value})
+  }
 
   render() {
-      const shouldBeReadOnly = function(rowData) {
-          return rowData['title'] != 'Mrs'
-      }
-
-      const colSpec = [
-          {title: 'Title', fieldName: 'title', inputType: "SelectField", selectOptions: ["Mr", "Mrs", "Miss", "Other"], width: 200, defaultValue: 'Mr'},
-          {title: 'Name', fieldName: 'foreName', inputType: "TextField", width: 200},
-          {title: 'Surname', fieldName: 'surname', inputType: "TextField", width: 200},
-          {title: 'Maiden Name', fieldName: 'maidenName', inputType: "TextField", width: 200, isReadOnly: shouldBeReadOnly},
-          {title: 'Employed', fieldName: 'employed', inputType: "Toggle", width: 200}
-      ]
-
-      const rowData = [
-          { title: 'Mr', foreName: 'John', surname: 'Smith', employed: true},
-          { title: 'Miss', foreName: 'Emily', surname: 'Lockhart', employed: false},
-          { title: 'Mrs', foreName: 'Marilyn', surname: 'Monroe', employed: true}
-      ]
-
-      const onChange = (dataTable) => {
-          console.log(dataTable)
-      }
-
-      return (
-      <MuiThemeProvider>
-          <MuiEditableTable
-              colSpec={colSpec}
-              rowData={rowData}
-              onChange={onChange}
-              reorderable={true}
-          />
-      </MuiThemeProvider>
+    return (
+      <div>
+        <Table
+          height={this.state.height}
+          fixedHeader={this.state.fixedHeader}
+          fixedFooter={this.state.fixedFooter}
+          selectable={this.state.selectable}
+          multiSelectable={this.state.multiSelectable}
+        >
+          <TableHeader
+            displaySelectAll={this.state.showCheckboxes}
+            adjustForCheckbox={this.state.showCheckboxes}
+            enableSelectAll={this.state.enableSelectAll}
+          >
+            <TableRow>
+              <TableHeaderColumn tooltip='The ID'>ID</TableHeaderColumn>
+              <TableHeaderColumn tooltip='The Name'>Name</TableHeaderColumn>
+              <TableHeaderColumn tooltip='The Status'>Status</TableHeaderColumn>
+            </TableRow>
+          </TableHeader>
+          <TableBody
+            displayRowCheckbox={this.state.showCheckboxes}
+            deselectOnClickaway={this.state.deselectOnClickaway}
+            showRowHover={this.state.showRowHover}
+            stripedRows={this.state.stripedRows}
+          >
+            {tableData.map( (row, index) => (
+              <TableRow key={index}>
+                <TableRowColumn>{index}</TableRowColumn>
+                <TableRowColumn>{row.name}</TableRowColumn>
+                <TableRowColumn>{row.status}</TableRowColumn>
+              </TableRow>
+              ))}
+          </TableBody>
+        </Table>
+      </div>
     )
   }
 }
