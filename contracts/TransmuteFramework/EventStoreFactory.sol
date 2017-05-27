a88049a9b7ea0acebc3a32871c5a43edc4a25291
Eric Olszewski <eolszewski@gmail.com>
2017-05-27 16:27:07 -0500

    Refactor (#3)
    * remove space
    
    * refactoring
    
    * step 2
    
    * Demo cut
    
    * ui cleanup
    
    * clean ui
    
    * clean names
    
    * Add updated Transmute Framework to Mercury.

--- contracts/TransmuteFramework/EventStoreFactory.sol
+++ contracts/TransmuteFramework/EventStoreFactory.sol
@@ -1,18 +1,22 @@
 pragma solidity ^0.4.8;
 import "./EventStore.sol";
 import "./SetLib/AddressSet/AddressSetLib.sol";
+import "./Utils/StringUtils.sol";
 
 contract EventStoreFactory is EventStore {
   using AddressSetLib for AddressSetLib.AddressSet;
-
-  mapping (address => address) creatorEventStoreMapping;
+  mapping (address => AddressSetLib.AddressSet) creatorEventStoreMapping;
   AddressSetLib.AddressSet EventStoreAddresses;
 
   // Fallback Function
   function() payable {}
 
   // Constructor
-  function EventStoreFactory() payable {}
+  function EventStoreFactory() payable {
+    uint eventIndex = solidityEventCount;
+    writeSolidityEvent('FACTORY_CREATED', 1, '');
+    writeSolidityEventProperty(eventIndex, 0, 'ContractOwnerAddress', 'Address', msg.sender, 0, '');
+  }
 
   // Modifiers
   modifier checkExistence(address _EventStoreAddress) {
@@ -22,10 +26,10 @@ contract EventStoreFactory is EventStore {
   }
 
   // Helper Functions
-  function getEventStoreByCreator() constant
-    returns (address)
+  function getEventStoresByCreator() constant
+    returns (address[])
   {
-    return creatorEventStoreMapping[msg.sender];
+    return creatorEventStoreMapping[msg.sender].values;
   }
 
   function getEventStores() constant
@@ -38,50 +42,39 @@ contract EventStoreFactory is EventStore {
 	function createEventStore() payable
     returns (address)
   {
-    // Validate Local State
-    if (creatorEventStoreMapping[msg.sender] != 0) {
-      throw;
-    }
-
-    // Update Local State
-
     // Interact With Other Contracts
 		EventStore _newEventStore = new EventStore();
 
     // Update State Dependent On Other Contracts
     EventStoreAddresses.add(address(_newEventStore));
-    creatorEventStoreMapping[msg.sender] = address(_newEventStore);
+    creatorEventStoreMapping[msg.sender].add(address(_newEventStore));
 
-    // Emit Events
     uint eventIndex = solidityEventCount;
 
-    writeSolidityEvent('EVENT_STORE_CREATED', 1, '0x0');
+    writeSolidityEvent('FACTORY_EVENT_STORE_CREATED', 2, '');
     writeSolidityEventProperty(eventIndex, 0, 'ContractAddress', 'Address', address(_newEventStore), 0, '');
-
-    eventIndex = solidityEventCount;
-
-    writeSolidityEvent('EVENT_STORE_AUDIT_LOG', 2, '0x1');
-    writeSolidityEventProperty(eventIndex, 0, 'ContractAddress', 'Address', address(_newEventStore), 0, '');
-    writeSolidityEventProperty(eventIndex, 1, 'ContractOwnerAddress', 'Address', address(msg.sender), 0, '');
+    writeSolidityEventProperty(eventIndex, 1, 'ContractOwnerAddress', 'Address', msg.sender, 0, '');
 
     return address(_newEventStore);
 	}
 
-  function killEventStore(address _address, address _creator)  {
+  function killEventStore(address _address) checkExistence(_address) {
     // Validate Local State
-    if ((_creator != msg.sender && this.owner() != msg.sender) || creatorEventStoreMapping[_creator] == 0) {
+    if (this.owner() != msg.sender || creatorEventStoreMapping[msg.sender].values.length == 0) {
       throw;
     }
 
     // Update Local State
-    delete creatorEventStoreMapping[_creator];
+    creatorEventStoreMapping[msg.sender].remove(_address);
     EventStoreAddresses.remove(_address);
 
     // Interact With Other Contracts
-    EventStore _EventStore = EventStore(_address);
-    _EventStore.kill();
+    EventStore _eventStore = EventStore(_address);
+    _eventStore.kill();
 
-    // Emit Events
-    // USE EVENT STORE
+    uint eventIndex = solidityEventCount;
+
+    writeSolidityEvent('FACTORY_EVENT_STORE_DESTROYED', 1, StringUtils.uintToBytes(eventIndex));
+    writeSolidityEventProperty(eventIndex, 0, 'ContractAddress', 'Address', _address, 0, '');
   }
 }
