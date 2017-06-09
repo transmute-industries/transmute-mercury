812a736c497555d7391015cb41fb9a07387a28bc
Orie Steele <orie@ohs.io>
2017-06-09 15:11:16 -0500

    fix web3 config page

--- src/components/Pages/Web3/Web3SettingsForm/Web3SettingsForm.js
+++ src/components/Pages/Web3/Web3SettingsForm/Web3SettingsForm.js
@@ -23,7 +23,13 @@ export const Web3SettingsForm = ({ web3, handleSubmit, submitForm, submitting })
       <Grid fluid>
         <Row className={classes.settingRow}>
           <h3>Web3 Provider</h3>
-          <Field style={{ width: '100%' }} name='provider' component={SelectField} hintText='Select a provider'>
+          <Field style={{ width: '100%' }} name='provider' component={SelectField} hintText='Select a provider'
+            onChange={event => {
+              setTimeout(() => {
+                handleSubmit()
+              }, 1 * 1000)
+
+            }}>
             <MenuItem value='testrpc' primaryText='Test RPC' />
             <MenuItem value='metamask' primaryText='MetaMask' />
             <MenuItem value='infura' primaryText='Infura' />
@@ -35,8 +41,8 @@ export const Web3SettingsForm = ({ web3, handleSubmit, submitForm, submitting })
           <Field style={{ width: '100%' }} name='defaultAddress' component={SelectField} hintText='Select a default Address'>
             {
               web3.addresses.map((address) => {
-              return <MenuItem key={address} value={address} primaryText={address}/>
-            })
+                return <MenuItem key={address} value={address} primaryText={address} />
+              })
             }
           </Field>
         </Row>
